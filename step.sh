c2372d7c0c473c4cada042a189df2dac7f1258e0
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-03-09 14:17:52 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -9,7 +9,10 @@ warnings_count=`echo $linter_report | grep -o 'WARNING' | wc -l`
 alarm_count=`echo $linter_report | grep -o 'ALARM' | wc -l`
 error_count=`echo $linter_report | grep -o 'ERROR' | wc -l`
 
-report= `echo {$error_count} {$alarm_count} {$warnings_count}`
+# report= `echo {$error_count} {$alarm_count} {$warnings_count}`
+
+report= `echo 3 4 5`
+
 envman add --key REPORT_ANALYSIS --value "$report"
 echo $report
 
